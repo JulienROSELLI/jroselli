acf103865e7c135a1a1f1b7b547eb37043133816
Julien ROSELLI <julien.roselli@euroerp.com>
2024-05-13 03:31:21 +0200

    novelle cerrection

--- components/canvas/CanvasLoader.tsx
+++ components/canvas/CanvasLoader.tsx
@@ -1,24 +1,24 @@
-"use client";
-import { Html, useProgress } from "@react-three/drei";
+'use client'
+import { Html, useProgress } from '@react-three/drei'
 
 const CanvasLoader = () => {
-  const { progress } = useProgress();
+  const { progress } = useProgress()
   return (
     <Html
-      as="div"
+      as='div'
       center
       style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
       }}
     >
-      <span className="canvas-loader"></span>
+      <span className='canvas-loader'></span>
       <p
         style={{
           fontSize: 14,
-          color: "#F1F1F1",
+          color: '#F1F1F1',
           fontWeight: 800,
           marginTop: 40,
         }}
@@ -26,7 +26,7 @@ const CanvasLoader = () => {
         {progress.toFixed(2)}%
       </p>
     </Html>
-  );
-};
+  )
+}
 
-export default CanvasLoader;
+export default CanvasLoader
